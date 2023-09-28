a0f70ecf49b22c35588388c128b63314d5e85a3f
EeeMoonJR <eeemoonart@gmail.com>
2023-09-28 22:35:29 +0300

    fix text splitting

--- tests/table.py
+++ tests/table.py
@@ -1,11 +1,31 @@
+from collections.abc import Iterator
 import re
+
+
+#TODO move to utils
+class ExceptDict(dict):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __getitem__(self, __key: object):
+        try:
+            res = super().__getitem__(__key)
+        except:
+            res = self.value
+
+        return res
+    
+
+class AlwaysIn(list):
+    def __contains__(self, __key: object) -> bool:
+        return True
 
 
 class Item:
     def __init__(self,
                  content: str,
-                 align: chr = 'l') -> None:
-        self.align: chr = align
+                 align: str = 'l') -> None:
+        self.align: str = align
 
         self._from_content(content)
 
@@ -23,19 +43,41 @@ class Item:
 
         return ''.join(content_list)
     
-    def edit(self, content: str):
-        self._from_content(content)
-        return self
+    def copy(self, content: str):
+        new = Item(content, self.align)
+        new.colors = self.colors
+        return new
 
 
 class Table:
-    def __init__(self, col_widths: dict[int, int] = None) -> None:
+    def __init__(self, col_widths: int | list[int] | dict[int, int] | None = None) -> None:
         self._rows: list[list[Item]] = []
-        self._col_widths: dict = col_widths
-        self._fixed_widths: list = list(col_widths.keys())
-        self._chars: list[chr] = [' ', '-', '|', '+']
+        self._chars: list[str] = [' ', '-', '|', '+']
+        self._num_cols: int = 0
 
-    def _fit_cols(self, items: list):
+        if isinstance(col_widths, int):
+            self._col_widths = ExceptDict(max(1, col_widths))
+            self._fixed_widths = AlwaysIn()
+
+        elif isinstance(col_widths, list):
+            self._fixed_widths = range(len(col_widths))
+            self._col_widths = {i: int(col_widths[i]) for i in self._fixed_widths}
+
+        elif isinstance(col_widths, dict):
+            self._col_widths = {int(i): int(col_widths[i]) for i in col_widths}
+            self._fixed_widths = list(col_widths.keys())
+
+        elif col_widths is None:
+            self._col_widths = ExceptDict(0)
+            self._fixed_widths = []
+
+        else:
+            raise ValueError("attribute 'col_widths' is of unsupported type")
+
+    def __getitem__(self, key: int):
+        return [i.content for i in self._rows[key]]
+
+    def _fit_cols(self, items: list[Item]):
         result = []
 
         for num, item in enumerate(items):
@@ -43,10 +85,10 @@ class Table:
             width = self._col_widths.get(num, 0)
 
             if width <= 0:
-                result.append([item.edit(content)])
+                result.append([item.copy(content)])
             else:
                 result.append(
-                    [item.edit(content[i:i+width]) for i in range(0, len(content), width)]
+                    [item.copy(content[i:i+width]) for i in range(0, len(content), width)]
                 )
 
         result = [
@@ -56,7 +98,9 @@ class Table:
 
         return [list(row) for row in zip(*result)]
 
-
+    def set_deco(self, chars: list):
+        self._chars = chars
+ 
     def add_row(self, items: list):
         row_items = [Item(str(i)) for i in items]
 
@@ -66,24 +110,25 @@ class Table:
         for num, item in enumerate(row_items):
             item_width = len(item.content)
 
-            if item_width > self._col_widths.get(num, 0) and num not in self._fixed_widths:
+            if isinstance(self._col_widths, ExceptDict):
+                self._col_widths[num] = self._col_widths.value
+            elif (item_width > self._col_widths.get(num, 0) 
+                and num not in self._fixed_widths):
                 self._col_widths[num] = item_width
 
-        max_row_width = max((len(i) for i in self._rows))
+        self._num_cols = max((len(i) for i in self._rows))
 
         for row in self._rows:
-            if len(row) < max_row_width:
-                row += [Item('')] * (max_row_width - len(row))
+            if len(row) < self._num_cols:
+                row += [Item('')] * (self._num_cols - len(row))
 
     def draw_lines(self):
         lines = []
-
         n_rows = len(self._rows)
-
         ph_c, hz_c, vt_c, cr_c = self._chars
 
         for num_row, row in enumerate(self._rows):
-            border = cr_c.join(hz_c * self._col_widths[w] for w in sorted(self._col_widths))
+            border = cr_c.join(hz_c * self._col_widths[w] for w in sorted(self._col_widths)[:self._num_cols])
             inner_border = vt_c + border + vt_c
             outer_border = cr_c + border + cr_c
 
@@ -95,7 +140,7 @@ class Table:
 
                 for num_item, item in enumerate(new_row):
                     content = item.formatted()
-                    free = max(0, self._col_widths.get(num_item, 0) - len(content))
+                    free = max(0, self._col_widths.get(num_item, 0) - len(item.content))
 
                     if item.align == "r":
                         content = ph_c * free + content
