1993edd3573b757828d6d4aead20d0a2a14d814e
EeeMoonJR <eeemoonart@gmail.com>
2023-09-28 22:35:29 +0300

    fix text splitting

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from weneda import FG
 from tests.table import Table
 
 
-t = Table({2: 5})
+t = Table(4)
 
 t.add_row([FG.BLUE+"111"+FG.DEFAULT, "2s131", "aa\naad"])
 t.add_row(["s", "sdfs"])
 t.add_row(["ad", None, "12ппппппппппппппппппп31", "gdfd"])
 
 
+with open('text.txt', 'w+') as f:
+    f.write(t.draw())
+    
 print(t.draw())
