e2204cd088092d16b3304ae22abeaa9ea03142cb
EeeMoonJR <eeemoonart@gmail.com>
2023-10-02 10:17:27 +0300

    added plceholder examples

--- examples/placeholders.py
+++ examples/placeholders.py
@@ -1,2 +1,13 @@
 from weneda import placeholder
 
+
+# Example 1: common usage
+@placeholder()
+def format_text(ph, **kwargs):
+    if ph == "name":
+        return "Alice"
+
+text = "Hello, {name}!"
+formatted = format_text(text)
+
+print(formatted)
