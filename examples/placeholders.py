ec4ea5a3c1ef13a1c50878cf53a5d02a88bbf309
EeeMoonJR <eeemoonart@gmail.com>
2024-08-05 13:38:27 +0300

    update example

--- examples/placeholders.py
+++ examples/placeholders.py
@@ -1,54 +1,25 @@
 import asyncio
-from weneda import placeholder
+from weneda import Placeholder
 
+
+
+class MyPlaceholder(Placeholder):
+    async def replace(self, placeholder: str, depth: int) -> str:
+        if placeholder.startswith('upper_'):
+            return placeholder.removeprefix('upper_').upper()
+        if placeholder == 'name':
+            return "Alex"
+        
 
 async def main():
-    # Example 1: common usage
-    @placeholder()
-    def format_text(ph: str, *, name: str = "someone"):
-        if ph == "name":
-            return name
+    # Example 1: Basic usage
+    ph = MyPlaceholder()
+    print(await ph.process("{upper_{name}}"))
 
-    text = "Hello, {name}!"
-
-    formatted = await format_text(text, name="Alice")
-    print("Example 1.1:", formatted) # Hello, Alice!
-
-    formatted = await format_text(text)
-    print("Example 1.2:", formatted) # Hello, someone!
+    # Example 2: Custom identifiers
+    custom = MyPlaceholder('$[', ']')
+    print(await custom.process("$[upper_$[name]]"))
 
 
-    # Example 2: custom identifiers
-    @placeholder("$[", "]")
-    def format_text(ph: str, **kwargs):
-        if ph == "amount":
-            return kwargs.get("amount", 0)
-
-    text = "This costs $[amount] USD"
-
-    formatted = await format_text(text, amount=15)
-    print("Example 2.1:", formatted) # This costs 15 USD
-
-
-    # Example 3: nested placeholders
-    @placeholder()
-    def format_text(ph: str, **kwargs):
-        if ph.startswith("num"):
-            return kwargs.get(ph, 0)
-        
-        if ph.startswith("sum_"):
-            action, *nums = ph.split("_")
-            return sum((int(i) for i in nums))
-
-    text = "{num1} + {num2} = {sum_{num1}_{num2}}"
-
-    formatted = await format_text(text, num1=3, num2=4)
-    print("Example 3.1:", formatted) # 3 + 4 = 7
-
-    text = "{num1} + {num2} + 5 + {num3} = {sum_{num1}_{num2}_5_{num3}}"
-
-    formatted = await format_text(text, num1=3, num2=4, num3=2)
-    print("Example 3.2:", formatted) # 3 + 4 + 5 + 2 = 14
-
-
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
